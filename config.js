46298c24908545d626fc7513005bf19d3bbb06a1
Marco Faraci <m.faraci@3technology.it>
2022-06-29 12:45:14 +0000

    create an executable cli

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url_download = "https://hook.eu1.make.com/be6cklfkd8kx9gxlrthdcb8rwpyvr3r1",
-    url_set_missing_key = "https://hook.eu1.make.com/jjl8a6l57y9oh84eq1xsgjt7qg1w8wd1?key="
+    url_download: "https://hook.eu1.make.com/be6cklfkd8kx9gxlrthdcb8rwpyvr3r1",
+    url_set_missing_key: "https://hook.eu1.make.com/jjl8a6l57y9oh84eq1xsgjt7qg1w8wd1?key="
 }
